2c0cd650039a11c65cd9e00825a217d4a7eb997f
devhenode <devodehenry.2000@gmail.com>
2024-01-17 01:45:59 +0100

    navbar almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Navbar />
-      <h1 className="text-3xl font-bold underline text-center">Hello React</h1>
+      {/* <h1 className="text-3xl font-bold underline text-center">Hello React</h1> */}
         
     </>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,41 @@
+import logo from "../assets/logo.png";
+
+// Desc: This is the navbar component for the website
 function Navbar() {
+  let Links = [
+    {name: "ABOUT",link: "#"},
+    {name: "TOKENOMICS",link: "#"},
+    {name: "ROADMAP",link: "#"},
+    {name: "QUOTES",link: "#"},
+    {name: "BUY TOKEN",link: "#"},
+    {name: "WHITEPAPER",link: "#"},
+  ];
   return (
-    <nav className=" relative flex w-full items-center ">
-      <li>ABOUT</li>
-      <li>TOKENOMICS</li>
-      <li>ROADMAP</li>
-      <li>QUOTES</li>
-      <li>BUY TOKEN</li>
-      <li>WHITEPAPER</li>
-    </nav>
+    <div>
+      <div className="shadow-md w-full fixed top-0 left-0">
+        <div className="md:flex items-center justify-between bg-green-950 py-3 md:px-5 px-5">
+          <div className="font-bold text-2xl cursor-pointer flex items-center">
+            <span>
+              <img src={logo} height={100} width={70} className="" />
+            </span>
+          </div>
+
+          <ul className="md:flex md:items-center">
+            {
+              Links.map((link, index) =>(
+                <li key={index} className="md:ml-8 text-md text-white hover:text-green-900 duration-500">
+                  <a href={link.link}>{link.name}</a>
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 }
-export default Navbar
+export default Navbar;
+
+// //  <ul className="">
+//
+// </ul>
