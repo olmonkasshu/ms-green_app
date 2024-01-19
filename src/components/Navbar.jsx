75d0bd4788ceee8e42a16aa7620811ee90aac69d
devhenode <devodehenry.2000@gmail.com>
2024-01-19 14:50:24 +0100

    nav now responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from "../assets/logo.png";
 import Button from "./Button";
-
+import { useState } from "react";
 function Navbar() {
   let Links = [
     { name: "ABOUT", link: "#" },
@@ -9,20 +9,30 @@ function Navbar() {
     { name: "ROADMAP", link: "#" },
     { name: "QUOTES", link: "#" },
   ];
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+  
+  const toggleMenu = () => {setIsMenuVisible(!isMenuVisible)};
 
   return (
     <div>
-      <div className="shadow-md w-full fixed top-0 left-0">
+      <div className="shadow-md w-full fixed flex-wrap top-0 left-0">
         <div className="md:flex items-center justify-between bg-green-950 py-3 md:px-5 px-5">
-          <div className="font-bold text-2xl cursor-pointer flex items-center">
+          <div className="flex flex-shrink-0 justify-between font-bold text-2xl cursor-pointer flex items-center">
             <span>
               <img src={logo} height={100} width={70} className="" alt="Logo" />
             </span>
+            <div onClick={toggleMenu} className="cursor-pointer">
+              <button className="px-3 py-2 border-2 rounded text-white border hover:text-white hover:border-white">
+                <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title>
+                  <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
+                </svg>
+              </button>
+            </div>
           </div>
-
-          <ul className="md:flex md:items-center space-x-4 mt-4 md:mt-0">
+          
+          <ul className={`md:block md:w-full md:flex md:items-center mt-4 md:mt-0 ${isMenuVisible ? '' : 'hidden'}`}>
             {Links.map((link, index) => (
-              <li key={index} className="md:ml-4 text-md text-white hover:text-green-900 duration-500">
+              <li key={index} className="md:ml-4 py-2 text-md text-white hover:text-green-900 duration-500">
                 <a href={link.link}>{link.name}</a>
               </li>
             ))}
