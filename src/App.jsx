95009aa2ca52a11879fd1becaec763eb2ea88367
olmonkasshu <olmontechdesign@gmail.com>
2024-01-20 11:34:08 +0100

    Doc Second page half done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import HeroPage from './components/Hero/HeroPage'
+import SecondPage from './components/Hero_2/SecondPage'
 
 function App() {
   return (
     <>
       <Navbar />
       <HeroPage />
+      <SecondPage/>
       {/* <h1 className="text-3xl font-bold underline text-center">Hello React</h1> */}
         
     </>
