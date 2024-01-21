91c5c3852ee9a61f10f0dfd5cc2fee31eb809e61
devhenode <devodehenry.2000@gmail.com>
2024-01-21 15:43:47 +0100

    nav responsive fully corrected

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,13 +15,13 @@ function Navbar() {
 
   return (
     <div>
-      <div className="shadow-md w-full fixed flex-wrap top-0 left-0">
-        <div className="md:flex items-center justify-between bg-green-950 py-3 md:px-5 px-5">
-          <div className="flex flex-shrink-0 justify-between font-bold text-2xl cursor-pointer flex items-center">
+      <div className="shadow-md w-full fixed top-0 left-0">
+        <div className="md:flex justify-between bg-green-950 py-3 md:px-5 px-5">
+          <div className="lg:flex flex-shrink-0 justify-between font-bold text-2xl cursor-pointer flex items-center">
             <span>
               <img src={logo} height={100} width={70} className="" alt="Logo" />
             </span>
-            <div onClick={toggleMenu} className="cursor-pointer">
+            <div onClick={toggleMenu} className="cursor-pointer md:hidden">
               <button className="px-3 py-2 border-2 rounded text-white border hover:text-white hover:border-white">
                 <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title>
                   <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
@@ -30,14 +30,14 @@ function Navbar() {
             </div>
           </div>
           
-          <ul className={`md:block md:w-full md:flex md:items-center mt-4 md:mt-0 ${isMenuVisible ? '' : 'hidden'}`}>
+          <ul className={`md:block md:w-full md:flex justify-end md:items-center mt-4 md:mt-0 ${isMenuVisible ? '' : 'hidden'}`}>
             {Links.map((link, index) => (
               <li key={index} className="md:ml-4 py-2 text-md text-white hover:text-green-900 duration-500">
                 <a href={link.link}>{link.name}</a>
               </li>
             ))}
             
-            <div className="md:flex md:ml-4 space-x-2">
+            <div className="md:flex md:ml-4">
               <Button>
                 BUY TOKEN
               </Button>
