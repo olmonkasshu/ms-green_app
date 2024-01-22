cbb4cbd3d3c645b11980459c8953fd893e7d1a21
olmonkasshu <olmontechdesign@gmail.com>
2024-01-22 16:58:46 +0100

    Docs fourthPage and footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import Navbar from './components/Navbar'
 import HeroPage from './components/Hero/HeroPage'
 import SecondPage from './components/Hero_2/SecondPage'
+import Footer from './components/Footer/Footer'
+import FourthPage from './components/Hero_4/FourthPage'
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Navbar />
       <HeroPage />
       <SecondPage/>
+      <FourthPage />
+      <Footer />
       {/* <h1 className="text-3xl font-bold underline text-center">Hello React</h1> */}
         
     </>
--- src/components/Hero_2/SecondPage.jsx
+++ src/components/Hero_2/SecondPage.jsx
@@ -6,25 +6,25 @@ const SecondPage = () => {
       {/* Left Column */}
       <div className="flex-1">
         {/* Add your text here */}
-        <p className=" p-11 font-extrabold text-lg mt-48">
+        <p className=" p-11 font-extrabold text-[40px] mt-32">
         The Sweetest Ride in <br /> Crypto
         </p>
       </div>
 
       {/* Green Line */}
-      <div className=" flex items-center justify-center h-screen w-1 bg-green-500 "></div>
+      <div className=" flex items-center justify-center h-[360px]  w-1 bg-green-500 mr-8"></div>
 
       {/* Right Column */}
       <div className="flex-1">
         {/* Add your text here */}
-        <p className="p-8">
+        <p className="pb-8">
         Get ready to be swept off your feet in the world of Candy Lovers, <br />where exhilarating 
         times are knocking at our door, and we can <br /> hardly contain our excitement as we share 
         the latest scoop with <br /> you! We're all about showering tribute upon the dazzling Ms.<br />
          Green while crafting a dynamic community that's buzzing with <br /> energy.
         </p>
 
-        <p className='p-6'>
+        <p>
           2023 has been a whirlwind adventure – we've taken centre stage <br /> in headlines, 
           and now we're gearing up to crank things up a <br /> notch. Brace yourself, because
           Ms $Green narrative has woven <br /> its magic, infusing our community with an electric 
@@ -35,6 +35,8 @@ const SecondPage = () => {
           WAITLIST
         </Button>
       </div>
+
+      
     </div>
   );
 };
