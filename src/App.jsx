9144a3a44b2309270568e3a0daa8dbb325217b11
devhenode <devodehenry.2000@gmail.com>
2024-01-22 18:13:31 +0100

    resolve conflict

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.css'
 import Navbar from './components/Navbar'
 import HeroPage from './components/Hero/HeroPage'
 import ThirdPage from "./components/ThirdPage"
-// import FifthPage from './components/FifthPage'
 
 function App() {
   return (
@@ -11,8 +10,7 @@ function App() {
       <HeroPage />
       {/* <h1 className="text-3xl font-bold underline text-center">Hello React</h1> */}
       <ThirdPage />
-
-      {/* <FifthPage /> */}
+     
     </>
   )
 }
