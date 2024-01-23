ea692c5631849a8545461e874fab377f13240fde
devhenode <devodehenry.2000@gmail.com>
2024-01-23 16:56:45 +0100

    anything

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 function Button(props) {
   return (
     <div className="px-0">
-      <button className="bg-gradient-to-r from-green-400 to-yellow-200 text-white px-3 py-2 m-2 md:text-sm rounded md:font-sm hover: from-white-500 hover:to-green-200">
+      <button className="ring-2 bg-green-900 ring-green-500 group-hovering:ring-1 transition duration-300 text-white px-3 py-2 m-2 md:text-sm rounded md:font-sm hover:bg-green-400">
         {props.children}
       </button>
       
