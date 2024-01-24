b419271b68c2e67e42c3890d799b046e4db4f980
devhenode <devodehenry.2000@gmail.com>
2024-01-24 22:15:16 +0100

    working on sixth page

--- src/components/SixthPage.jsx
+++ src/components/SixthPage.jsx
@@ -1,80 +1,82 @@
 /* eslint-disable react/no-unescaped-entities */
 
-function SixthPage() { 
+function SixthPage() {
   return (
-    <div className="bg-green-900 mt-0">
+    <div className="bg-green-900 mt-0 md:w-full lg:w-3/4 xl:w-full 2xl:w-1/3">
       <div className="px-5 py-5">
         <div>
           <article className="text-wrap py-10">
-            <h1 className="font-bold text-6xl text-center text-lime-400">Roadmap</h1>
-            <p className="text-center text-1xl text-white">
-              Navigating the Candyland Adventure; 
-              Whimsical Roadmap.
+            <h1 className="font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-center text-lime-400">
+              Roadmap
+            </h1>
+            <p className="text-center text-sm sm:text-base md:text-lg lg:text-xl text-white">
+              Navigating the Candyland Adventure; Whimsical Roadmap.
             </p>
           </article>
-          
-          <p className="text-center text-1xl font-thin text-white">Discover where the journey takes us, from 
-            forging partnerships with fellow meme-makers 
-            to the tempting possibilities of NFT 
-            (Notable Flavorful Tokens) integration. .
+
+          <p className="text-center text-sm sm:text-base md:text-lg lg:text-xl font-thin text-white">
+            Discover where the journey takes us, from forging partnerships with
+            fellow meme-makers to the tempting possibilities of NFT (Notable
+            Flavorful Tokens) integration. .
           </p>
         </div>
 
-        <div className="bg-green-950 ring-2 ring-lime-400 ml-40 mt-10 justify-between h-dvh w-3/5 p-5">
-          <div className=" flex p-7 text-lime-400">
-            <span className="mt-2 px-5">
-              Q1 2024
-            </span>
+        <div className="bg-green-950 ring-2 ring-lime-400 ml-10 sm:ml-5 md:ml-20 lg:ml-40 mt-10 :text-1xl justify-between lg:h-dvh  sm:w-4/5  md:w-3/5 p-5">
+          <div className="grid flex-col sm:flex-row p-7 text-lime-400">
+            <span className="mt-2 px-5">Q1 2024</span>
 
-            <div>
-             <span className="grid text-lime-400 text-4xl">Ms $Green's:</span>
-              <ul className="col text-white">
-                <li>Launch the Ms $green Token with a sprinkle of
-                  magic and a whole lot of meme-worthy charm.
+            <div className="mb-2">
+              <span className="grid text-lime-400 text-2xl sm:text-3xl md:text-4xl">
+                Ms $Green's:
+              </span>
+              <ul className="col text-white text-xs sm:text-sm md:text-base lg:text-lg">
+                <li>
+                  Launch the Ms $green Token with a sprinkle of magic and a
+                  whole lot of meme-worthy charm.
                 </li>
                 <li>
-                  Set the stage for an unforgettable journey that's sweeter than candy.
+                  Set the stage for an unforgettable journey that's sweeter than
+                  candy.
                 </li>
               </ul>
             </div>
-          </div>
 
-          <div className="flex p-7 text-lime-400">
-            <span className="mt-2 px-5">
-              Q1 2024
-            </span>
-
-            <div>
-             <span className="grid text-lime-400 text-4xl">Ms $Green's:</span>
-              <ul className="col text-white">
-                <li>Launch the Ms $green Token with a sprinkle of
-                  magic and a whole lot of meme-worthy charm.
+            <div className="mb-2">
+              <span className="grid text-lime-400 text-2xl sm:text-3xl md:text-4xl">
+                Ms $Green's:
+              </span>
+              <ul className="col text-white text-xs sm:text-sm md:text-base lg:text-lg">
+                <li>
+                  Launch the Ms $green Token with a sprinkle of magic and a
+                  whole lot of meme-worthy charm.
                 </li>
                 <li>
-                  Set the stage for an unforgettable journey that's sweeter than candy.
+                  Set the stage for an unforgettable journey that's sweeter than
+                  candy.
                 </li>
               </ul>
             </div>
-          </div>
 
-          <div className="flex p-7 text-lime-400">
-            <span className="mt-2 px-5">
-              Q1 2024
-            </span>
-
-            <div>
-             <span className="grid text-lime-400 text-4xl">Ms $Green's:</span>
-              <ul className="col text-white">
-                <li>Launch the Ms $green Token with a sprinkle of
-                  magic and a whole lot of meme-worthy charm.
+            <div className="mb-0">
+              <span className="grid text-lime-400 text-2xl sm:text-3xl md:text-4xl">
+                Ms $Green's:
+              </span>
+              <ul className="col text-white text-xs sm:text-sm md:text-base lg:text-lg">
+                <li>
+                  Launch the Ms $green Token with a sprinkle of magic and a
+                  whole lot of meme-worthy charm.
                 </li>
                 <li>
-                  Set the stage for an unforgettable journey that's sweeter than candy.
+                  Set the stage for an unforgettable journey that's sweeter than
+                  candy.
                 </li>
               </ul>
             </div>
+            
           </div>
         </div>
+
+        
       </div>
     </div>
   );
