0951aa273c1a565548cd48610c283e87efe76c8f
devhenode <devodehenry.2000@gmail.com>
2024-01-29 14:15:48 +0100

    new week debusgging sixth page

--- src/components/Slider/Slider_1.jsx
+++ src/components/Slider/Slider_1.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 
-<<<<<<< HEAD:src/components/SixthPage.jsx
 function SixthPage() {
   return (
     <div className="bg-green-900 mt-0 md:w-full lg:w-3/4 xl:w-full 2xl:w-1/3">
@@ -28,66 +27,3 @@ function SixthPage() {
 }
 
 export default SixthPage;
-=======
-const Slider_1 = () => {
-  return (
-    <div className="">
-          <div className=" flex p-7 text-lime-400">
-            <span className="mt-2 px-5">
-              Q1 2024
-            </span>
-
-            <div>
-             <span className="grid text-lime-400 text-4xl">Ms $Green's:</span>
-              <ul className="col text-white">
-                <li>Launch the Ms $green Token with a sprinkle of
-                  magic and a whole lot of meme-worthy charm.
-                </li>
-                <li>
-                  Set the stage for an unforgettable journey that's sweeter than candy.
-                </li>
-              </ul>
-            </div>
-          </div>
-
-          <div className="flex p-7 text-lime-400">
-            <span className="mt-2 px-5">
-              Q1 2024
-            </span>
-
-            <div>
-             <span className="grid text-lime-400 text-4xl">Ms $Green's:</span>
-              <ul className="col text-white">
-                <li>Launch the Ms $green Token with a sprinkle of
-                  magic and a whole lot of meme-worthy charm.
-                </li>
-                <li>
-                  Set the stage for an unforgettable journey that's sweeter than candy.
-                </li>
-              </ul>
-            </div>
-          </div>
-
-          <div className="flex p-7 text-lime-400">
-            <span className="mt-2 px-5">
-              Q1 2024
-            </span>
-
-            <div>
-             <span className="grid text-lime-400 text-4xl">Ms $Green's:</span>
-              <ul className="col text-white">
-                <li>Launch the Ms $green Token with a sprinkle of
-                  magic and a whole lot of meme-worthy charm.
-                </li>
-                <li>
-                  Set the stage for an unforgettable journey that's sweeter than candy.
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-  )
-}
-
-export default Slider_1
->>>>>>> facd4dfb4ae3494de96dd56daa63394aed881ac1:src/components/Slider/Slider_1.jsx
