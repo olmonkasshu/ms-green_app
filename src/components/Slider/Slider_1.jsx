13f66db7959593cce8d24f2d33fffe64b60b61d1
olmonkasshu <olmontechdesign@gmail.com>
2024-01-31 14:42:10 +0100

    corrections on sixthpage

--- src/components/Slider/Slider_1.jsx
+++ src/components/Slider/Slider_1.jsx
@@ -1,29 +1,63 @@
 import React from 'react'
 
-function SixthPage() {
+const Slider_1 = () => {
   return (
-    <div className="bg-green-900 mt-0 md:w-full lg:w-3/4 xl:w-full 2xl:w-1/3">
-      <div className="px-5 py-5">
-        <div>
-          <article className="text-wrap py-10">
-            <h1 className="font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-center text-lime-400">
-              Roadmap
-            </h1>
-            <p className="text-center text-sm sm:text-base md:text-lg lg:text-xl text-white">
-              Navigating the Candyland Adventure; Whimsical Roadmap.
-            </p>
-          </article>
+    <div className="">
+          <div className=" flex p-7 text-lime-400">
+            <span className="mt-2 px-5">
+              Q1 2024
+            </span>
 
-          <p className="text-center text-sm sm:text-base md:text-lg lg:text-xl font-thin text-white">
-            Discover where the journey takes us, from forging partnerships with
-            fellow meme-makers to the tempting possibilities of NFT (Notable
-            Flavorful Tokens) integration. .
-          </p>
+            <div>
+             <span className="grid text-lime-400 text-4xl">Ms $Green's:</span>
+              <ul className="col text-white">
+                <li>Launch the Ms $green Token with a sprinkle of
+                  magic and a whole lot of meme-worthy charm.
+                </li>
+                <li>
+                  Set the stage for an unforgettable journey that's sweeter than candy.
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="flex p-7 text-lime-400">
+            <span className="mt-2 px-5">
+              Q1 2024
+            </span>
+
+            <div>
+             <span className="grid text-lime-400 text-4xl">Ms $Green's:</span>
+              <ul className="col text-white">
+                <li>Launch the Ms $green Token with a sprinkle of
+                  magic and a whole lot of meme-worthy charm.
+                </li>
+                <li>
+                  Set the stage for an unforgettable journey that's sweeter than candy.
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="flex p-7 text-lime-400">
+            <span className="mt-2 px-5">
+              Q1 2024
+            </span>
+
+            <div>
+             <span className="grid text-lime-400 text-4xl">Ms $Green's:</span>
+              <ul className="col text-white">
+                <li>Launch the Ms $green Token with a sprinkle of
+                  magic and a whole lot of meme-worthy charm.
+                </li>
+                <li>
+                  Set the stage for an unforgettable journey that's sweeter than candy.
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-        
-      </div>
-    </div>
-  );
+  )
 }
 
-export default SixthPage;
+export default Slider_1
