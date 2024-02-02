bc34e284372b8938216ebf1f300062112b2b560f
vctcode <victorode01@gmail.com>
2024-02-02 20:32:14 +0100

    style: fixed nav-index UI bug

--- src/components/Hero_5/FifthPage.jsx
+++ src/components/Hero_5/FifthPage.jsx
@@ -3,32 +3,39 @@ import Tokenomics from "../../assets/tokenomics.png";
 
 const FifthPage = () => {
   return (
-    <div className="mt-3 bg-green-900">
+    <div className="mt-3 px-20 bg-green-900">
       <p className="font-extrabold text-[40px] text-center pt-8 text-green-400">Tokenomics</p>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        <div className="bg-white-800 mt-20 py-14 text-center md:text-left mx-52 md:mx-0"> {/* Added text-center for mobile */}
-          <div className="flex items-center justify-center w-6 h-6 bg-yellow-600 rounded-full mb-10 col-span-1 md:ml-20">
-            <span className="text-green-600 pl-20 ">Pre-sale</span>
+      <div className="flex justify-between items-center gap-4">
+        <div className="bg-white-800 mt-20 py-14 text-center md:text-left mx-52 md:mx-0">
+          {/* Added text-center for mobile */}
+          <div className="flex gap-4 ">
+            <div className="flex items-center justify-center w-6 h-6 bg-yellow-600 rounded-full mb-10 col-span-1 md:ml-20"></div>
+            <span className="text-green-600">Pre-sale</span>
           </div>
 
-          <div className="flex items-center justify-center w-6 h-6 bg-orange-600 rounded-full mb-10 col-span-1 md:ml-20">
-            <span className="text-green-600 pl-28 ">Liquidity Supply</span>
+          <div className="flex gap-4 ">
+            <div className="flex items-center justify-center w-6 h-6 bg-orange-600 rounded-full mb-10 col-span-1 md:ml-20"></div>
+            <span className="text-green-600">Liquidity Supply</span>
           </div>
 
-          <div className="flex items-center justify-center w-6 h-6 bg-yellow-400 rounded-full mb-10 col-span-1 md:ml-20">
-            <span className="text-green-600 pl-36">Aggressive Marketing</span>
+          <div className="flex gap-4 ">
+            <div className="flex items-center justify-center w-6 h-6 bg-yellow-400 rounded-full mb-10 col-span-1 md:ml-20"></div>
+            <span className="text-green-600">Aggressive Marketing</span>
           </div>
 
-          <div className="flex items-center justify-center w-6 h-6 bg-blue-500 rounded-full mb-10 col-span-1 md:ml-20">
-            <span className="text-green-600 pl-28">Team 15 Month locked</span>
+          <div className="flex gap-4 ">
+            <div className="flex items-center justify-center w-6 h-6 bg-blue-500 rounded-full mb-10 col-span-1 md:ml-20"></div>
+            <span className="text-green-600">Team 15 Month locked</span>
           </div>
 
-          <div className="flex items-center justify-center w-6 h-6 bg-blue-300 rounded-full mb-10 col-span-1 md:ml-20">
-            <span className="text-green-600 pl-28">Airdrop</span>
+          <div className="flex gap-4 ">
+            <div className="flex items-center justify-center w-6 h-6 bg-blue-300 rounded-full mb-10 col-span-1 md:ml-20"></div>
+            <span className="text-green-600">Airdrop</span>
           </div>
 
-          <div className="flex items-center justify-center w-6 h-6 bg-green-300 rounded-full mb-10 col-span-1 md:ml-20">
-            <span className="text-green-600 pl-36">Community Reward</span>
+          <div className="flex gap-4 ">
+            <div className="flex items-center justify-center w-6 h-6 bg-green-300 rounded-full mb-10 col-span-1 md:ml-20"></div>
+            <span className="text-green-600">Community Reward</span>
           </div>
         </div>
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,12 +10,14 @@ function Navbar() {
     { name: "QUOTES", link: "#" },
   ];
   const [isMenuVisible, setIsMenuVisible] = useState(false);
-  
-  const toggleMenu = () => {setIsMenuVisible(!isMenuVisible)};
+
+  const toggleMenu = () => {
+    setIsMenuVisible(!isMenuVisible);
+  };
 
   return (
     <div>
-      <div className="shadow-md w-full fixed top-0 left-0">
+      <div className="shadow-md w-full fixed z-10 top-0 left-0">
         <div className="md:flex justify-between bg-green-950 py-3 md:px-5 px-5">
           <div className="lg:flex flex-shrink-0 justify-between font-bold text-2xl cursor-pointer flex items-center">
             <span>
@@ -23,27 +25,32 @@ function Navbar() {
             </span>
             <div onClick={toggleMenu} className="cursor-pointer md:hidden">
               <button className="px-3 py-2 border-2 rounded text-white hover:text-white hover:border-white">
-                <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title>
-                  <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
+                <svg
+                  className="fill-current h-3 w-3"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <title>Menu</title>
+                  <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
                 </svg>
               </button>
             </div>
           </div>
-          
-          <ul className={`md:block md:w-full md:flex justify-end md:items-center mt-4 md:mt-0 ${isMenuVisible ? '' : 'hidden'}`}>
+
+          <ul
+            className={`md:w-full md:flex justify-end md:items-center mt-4 md:mt-0 ${
+              isMenuVisible ? "" : "hidden"
+            }`}>
             {Links.map((link, index) => (
-              <li key={index} className="md:ml-4 py-2 text-md text-white hover:text-green-900 duration-500">
+              <li
+                key={index}
+                className="md:ml-4 py-2 text-md text-white hover:text-green-900 duration-500">
                 <a href={link.link}>{link.name}</a>
               </li>
             ))}
-            
+
             <div className="md:flex md:ml-4">
-              <Button>
-                BUY TOKEN
-              </Button>
-              <Button>
-                WHITEPAPER
-              </Button>
+              <Button>BUY TOKEN</Button>
+              <Button>WHITEPAPER</Button>
             </div>
           </ul>
         </div>
