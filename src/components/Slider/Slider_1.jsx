69c8be341a49d440038aa1f5b800bc93cc05bc21
olmonkasshu <olmontechdesign@gmail.com>
2024-02-02 15:35:13 +0100

    all changes done

--- src/components/Slider/Slider_1.jsx
+++ src/components/Slider/Slider_1.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 
 const Slider_1 = () => {
   return (
-    <div className="overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;">
+    <div className="">
           <div className=" flex p-7 text-lime-400">
             <span className="mt-2 px-5">
               Q1 2024
@@ -13,13 +11,9 @@ const Slider_1 = () => {
             <div>
              <span className="grid text-lime-400 text-4xl">Ms $Green's:</span>
               <ul className="col text-white">
-            
-              <li >Launch the Ms $green Token with a sprinkle of
+                <li>Launch the Ms $green Token with a sprinkle of
                   magic and a whole lot of meme-worthy charm.
-
                 </li>
-              
-                
                 <li>
                   Set the stage for an unforgettable journey that's sweeter than candy.
                 </li>
@@ -33,14 +27,6 @@ const Slider_1 = () => {
             </span>
 
             <div>
-<<<<<<< HEAD
-             <span className="grid text-lime-400 text-4xl">The Great Meme Bake-off:- Ongoing </span>
-              <ul className="col text-white">
-                <li>Stir up excitement with our first-ever meme creation contest. 
-                </li>
-                <li>
-                  Watch the community knead creativity into dough and bake up some meme-tastic treats
-=======
              <span className="grid text-lime-400 text-4xl">Ms $Green's:</span>
               <ul className="col text-white">
                 <li>Launch the Ms $green Token with a sprinkle of
@@ -48,7 +34,6 @@ const Slider_1 = () => {
                 </li>
                 <li>
                   Set the stage for an unforgettable journey that's sweeter than candy.
->>>>>>> 13f66db7959593cce8d24f2d33fffe64b60b61d1
                 </li>
               </ul>
             </div>
@@ -60,14 +45,6 @@ const Slider_1 = () => {
             </span>
 
             <div>
-<<<<<<< HEAD
-             <span className="grid text-lime-400 text-4xl">The Great meme Bake-off </span>
-              <ul className="col text-white">
-                <li>Stir up excitement with our first ever meme creation contest.
-                </li>
-                <li>
-                  Watch the community knead creativity into dough and bake up some meme-tastic treats
-=======
              <span className="grid text-lime-400 text-4xl">Ms $Green's:</span>
               <ul className="col text-white">
                 <li>Launch the Ms $green Token with a sprinkle of
@@ -75,7 +52,6 @@ const Slider_1 = () => {
                 </li>
                 <li>
                   Set the stage for an unforgettable journey that's sweeter than candy.
->>>>>>> 13f66db7959593cce8d24f2d33fffe64b60b61d1
                 </li>
               </ul>
             </div>
--- src/components/Slider/Slider_3.jsx
+++ src/components/Slider/Slider_3.jsx
@@ -4,7 +4,6 @@ const Slider_3 = () => {
     <div className="">
           <div className=" flex p-7 text-lime-400">
             <span className="mt-2 px-5">
-<<<<<<< HEAD
               Q2 2024
             </span>
 
@@ -30,37 +29,6 @@ const Slider_3 = () => {
                 </li>
                 <li>
                   It's like a potluck, but with memes (and fewer calories).
-=======
-            Q1 2025
-            </span>
-
-            <div>
-             <span className="grid text-lime-400 text-4xl">M&M's Moonbase</span>
-              <ul className="col text-white">
-                <li>LEstablish the first-ever meme-themed moonbase, <br />
-                where gravity is just a state of mind.
-                </li>
-                <li>
-                Crypto meets lunar laughter in this historic leap for <br /> memekind.
-                </li>
-              </ul>
-            </div>
-          </div>
-
-
-          <div className="flex p-7 text-lime-400">
-            <span className="mt-2 px-5">
-              Q1 2025
-            </span>
-
-            <div>
-             <span className="grid text-lime-400 text-4xl">Galactic Giggle Network</span>
-              <ul className="col text-white">
-                <li>Spread meme joy across the galaxy with our interstellar <br /> communication platform.
-                </li>
-                <li>
-                Memes: the universal language that unites civilizations <br />far and wide.
->>>>>>> c9f2f299819f76699a7157f7b8e0e01eae8c5fa8
                 </li>
               </ul>
             </div>
@@ -72,7 +40,6 @@ const Slider_3 = () => {
             </span>
 
             <div>
-<<<<<<< HEAD
              <span className="grid text-lime-400 text-4xl">laughter Liquidity Pool</span>
               <ul className="col text-white">
                 <li>
@@ -80,23 +47,10 @@ const Slider_3 = () => {
                 </li>
                 <li>
                   Because just like a bag of M&M's, our liquidity should be perfectly balance
-=======
-             <span className="grid text-lime-400 text-3xl">Cosmic Comedy Tour: - Q3 2025</span>
-              <ul className="col text-white">
-                <li>Take the show on the road and host meme events on  <br />planets, asteroids, and beyond.
-                </li>
-                <li>
-                Bringing laughter to the final frontier, one meme at a <br /> time.
->>>>>>> c9f2f299819f76699a7157f7b8e0e01eae8c5fa8
                 </li>
               </ul>
             </div>
           </div>
-<<<<<<< HEAD
-=======
-
-          
->>>>>>> c9f2f299819f76699a7157f7b8e0e01eae8c5fa8
         </div>
   )
 }
